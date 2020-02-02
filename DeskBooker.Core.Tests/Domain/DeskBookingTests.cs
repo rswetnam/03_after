5f36c0fbccff9006cad3cad0661c941f218fea4c
rswetnam <rswetnam@Check30.onmicrosoft.com>
2020-02-02 11:31:50 -0800

    Add project files.

---
+++ DeskBooker.Core.Tests/Domain/DeskBookingTests.cs
@@ -0,0 +1,89 @@
+﻿using DeskBooker.Core.DataInterface;
+using DeskBooker.Core.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DeskBooker.Core.Domain
+{
+    public class DeskBookingTests
+    {
+        private readonly DeskBookingRequest _request;
+        private readonly List<Desk> _availableDesks;
+        private readonly Mock<IDeskBookingRepository> _deskBookingRepositoryMock;
+        private readonly Mock<IDeskRepository> _deskRepositoryMock;
+        private readonly DeskBooking _booking;
+        //private readonly DeskBookingRequestProcessor _processor;
+
+        public DeskBookingTests()
+        {
+            _request = new DeskBookingRequest
+            {
+                FirstName = "Thomas",
+                LastName = "Huber",
+                Email = "thomas@thomasclaudiushuber.com",
+                Date = new DateTime(2020, 1, 28)
+            };
+
+            _availableDesks = new List<Desk> { new Desk { Id = 7 } };
+
+            _deskBookingRepositoryMock = new Mock<IDeskBookingRepository>();
+            _deskRepositoryMock = new Mock<IDeskRepository>();
+            _deskRepositoryMock.Setup(x => x.GetAvailableDesks(_request.Date))
+              .Returns(_availableDesks);
+            _booking = new DeskBooking();
+
+            //_processor = new DeskBookingRequestProcessor(_deskBookingRepositoryMock.Object, _deskRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => _booking.CreateBooking(null));
+
+            Assert.Equal("request", exception.ParamName);
+        }
+
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            // Act
+            DeskBooking result = _booking.CreateBooking(_request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(_request.FirstName, result.FirstName);
+            Assert.Equal(_request.LastName, result.LastName);
+            Assert.Equal(_request.Email, result.Email);
+            Assert.Equal(_request.Date, result.Date);
+        }
+
+        [Fact]
+        public void ShouldSaveDeskBooking()
+        {
+            DeskBooking savedDeskBooking = null;
+            _deskBookingRepositoryMock.Setup(x => x.Save(It.IsAny<DeskBooking>()))
+              .Callback<DeskBooking>(deskBooking =>
+              {
+                  savedDeskBooking = deskBooking;
+              });
+
+            _booking.CreateBooking(_request);
+
+            // This fails - I get error:  System.NullReferenceException : Object reference not set to an instance of an object.
+            // Can't figure out why
+            _deskBookingRepositoryMock.Verify(x => x.Save(It.IsAny<DeskBooking>()), Times.Once);
+
+            Assert.NotNull(savedDeskBooking);
+            Assert.Equal(_request.FirstName, savedDeskBooking.FirstName);
+            Assert.Equal(_request.LastName, savedDeskBooking.LastName);
+            Assert.Equal(_request.Email, savedDeskBooking.Email);
+            Assert.Equal(_request.Date, savedDeskBooking.Date);
+            Assert.Equal(_availableDesks.First().Id, savedDeskBooking.DeskId);
+        }
+
+
+    }
+}
---
+++ DeskBooker.Core/Domain/DeskBooking.cs
@@ -0,0 +1,56 @@
+﻿using DeskBooker.Core.DataInterface;
+using System;
+
+namespace DeskBooker.Core.Domain
+{
+    public class DeskBooking : DeskBookingBase
+    {
+        private readonly IDeskBookingRepository _deskBookingRepository;
+
+        public DeskBooking()
+        {
+        }
+
+        public DeskBooking(IDeskBookingRepository deskBookingRepository)
+        {
+            _deskBookingRepository = deskBookingRepository;
+        }
+
+
+        public int Id { get; set; }
+        public int DeskId { get; set; }
+
+        public void CreateBooking()
+        {
+
+        }
+
+        public DeskBooking CreateBooking(DeskBookingRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            var deskBooking = Create<DeskBooking>(request);
+
+            _deskBookingRepository.Save(deskBooking);
+
+            return deskBooking;
+        }
+
+        private static T Create<T>(DeskBookingRequest request) where T : DeskBookingBase, new()
+        {
+            return new T
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date
+            };
+        }
+
+
+
+    }
+}
